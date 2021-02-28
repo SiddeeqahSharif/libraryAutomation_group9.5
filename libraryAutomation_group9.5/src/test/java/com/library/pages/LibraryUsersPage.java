0ce6127ba00761a698408d6ee9d81a20b4ea25f0
Siddeeqah Sharif Fichman <siddeeqah.sharif@gmail.com>
2021-02-28 17:59:12 -0500

    fixed issues with feature and Scenario and step definitions for UserStory 4

--- libraryAutomation_group9.5/src/test/java/com/library/pages/LibraryUsersPage.java
+++ libraryAutomation_group9.5/src/test/java/com/library/pages/LibraryUsersPage.java
@@ -33,8 +33,9 @@ public class LibraryUsersPage extends Library_BasePage{
     @FindBy(xpath = "//div[@class='toast-message']")
     public WebElement UserAddSuccessMessage;
 
-    @FindBy(xpath = "//a[@onclick='Users.edit_user(3918)']")
+    @FindBy(xpath = "//a[@class='btn btn-primary btn-sm']")
     public WebElement EditUserButton;
+
 
 
 
