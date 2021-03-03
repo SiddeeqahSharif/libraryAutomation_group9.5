b07e0c07333cdc03654684999443e88aea311e7f
Akilah Rabb <akilahrabb@gmail.com>
2021-03-03 17:20:39 -0500

    Utilities update

--- libraryAutomation_group9.5/src/test/java/com/library/utilities/BrowserUtils.java
+++ libraryAutomation_group9.5/src/test/java/com/library/utilities/BrowserUtils.java
@@ -46,19 +46,20 @@ in browsers, and store in this class.
 
 
 
-        //create method name: wait
-        //converting milliseconds to seconds
-        //handle checked exception
+    //create method name: wait
+    //converting milliseconds to seconds
+    //handle checked exception
 
-        public static void sleep(int second) {
-            second *= 1000;
-            try {
-                Thread.sleep(second);
-            } catch (InterruptedException e) {
-                System.out.println("something happened in sleep method");
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("something happened in sleep method");
 
-            }
         }
+    }
 
 
 }
+
